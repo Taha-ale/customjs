3fe9919cb413fad9baa4545fe02429816144f114
Taha-ale <130196866+Taha-ale@users.noreply.github.com>
2023-04-08 07:11:54 +0500

    Update customcodeinvoice.js

--- customcodeinvoice.js
+++ customcodeinvoice.js
@@ -1,3 +1,3 @@
-$('.welcome-text').each(function() {
-        $(this).insertBefore($(this).parent().find('.subject'));
+jQuery('.welcome-text').each(function() {
+        jQuery(this).insertBefore(jQuery(this).parent().find('.subject'));
 });
